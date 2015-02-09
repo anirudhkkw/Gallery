121b3ac1c81830098052cb2d9704138d1cde356b
Anirudh Kalkotwar <anirudh.kkw@gmail.com>
2015-02-09 13:47:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-mongoose.connect('mongodb://127.0.0.1:27017/FlickrDb');
+mongoose.connect('mongodb://taher:cialfo@ds041177.mongolab.com:41177/flickrdb');
 app.use('/', routes);
 app.use('/index', routes);
 app.use('/users', users);
